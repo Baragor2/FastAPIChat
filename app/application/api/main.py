4801e05f3cd1a4f49ea194302fa17de371210a0e
Yauheni Mukhin <evgen3email@gmail.com>
2024-10-27 13:13:32 +0300

    main file fix

--- app/application/api/main.py
+++ app/application/api/main.py
@@ -4,6 +4,6 @@ from fastapi import FastAPI
 def create_app():
     return FastAPI(
         title='FastAPIChat',
-        docs_url='api/docs',
+        docs_url='/api/docs',
         description='Chat on FastAPI with DDD architecture',
     )
