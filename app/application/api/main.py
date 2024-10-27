7f587353881c37dc258d04a2a4fff354bdd1c147
Yauheni Mukhin <evgen3email@gmail.com>
2024-10-27 12:01:07 +0300

    base app

---
+++ app/application/api/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+
+def create_app():
+    return FastAPI(
+        title='FastAPIChat',
+        docs_url='api/docs',
+        description='Chat on FastAPI with DDD architecture',
+    )
